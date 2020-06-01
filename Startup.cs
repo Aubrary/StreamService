3c6646dc45c0bd50c88ce9cc3dd6927cc0198d93
Benjamin Mlynek <benjaminmlynek@Benjamins-MacBook-Pro.local>
2020-06-01 17:30:56 +0200

    Fixed error in swagger url

--- Startup.cs
+++ Startup.cs
@@ -68,7 +68,7 @@ namespace StreamService
             app.UseSwagger();
             app.UseSwaggerUI(c =>
             {
-                c.SwaggerEndpoint("streamservice/swagger/v1/swagger.json", "StreamService API V1");
+                c.SwaggerEndpoint("/streamservice/swagger/v1/swagger.json", "StreamService API V1 Traefik");
                 c.SwaggerEndpoint("/swagger/v1/swagger.json", "StreamService API V1");
             });
             app.UseAuthentication();
